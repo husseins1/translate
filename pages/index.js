bf67270d5cbcfc659272f0a4c3d274b62bf2ef03
Hussein <hussendon3@gmail.com>
2022-07-17 15:27:20 +0300

    using big object now

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,235 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import { useState } from "react";
 import { useTransition } from "react";
-import words from "../words/index"
+import words from "../words/index";
 import Fuse from "fuse.js";
-const terms = [
-  'ابتسم',
-  'احترم',
-  'اخذ',
-  'اعتنى',
-  'اعطى',
-  'انتبه',
-  'تذكر',
-  'راى',
-  'فكر',
-  'نظر',
+
+const populateObj = (paths, folderName) => {
+  let obj = {};
+  paths.forEach(
+    (ele) =>
+      (obj[
+        ele.replace(/\.(png|jpe?g|svg|jpg|JPG)$/, "")
+      ] = `${folderName}/${ele}`)
+  );
+  return obj;
+};
+const categories = [
+  "ابجدية اللغة الانكليزية",
+  "الاشهر والايام والوقت",
+  "الافعال",
+  "البيت ومحتوياته",
+  "الحيوانات والطيور والحشرات",
+  "الخضروات",
+  "الرياضة",
+  "العلاقات الاجتماعية والتحية والسلام",
+  "العلوم والظواهر الطبيعية",
+  "الفنون",
+  "القواعد والابجدية",
+  "الكمبيوتر",
+  "المدرسة ومستلزماتها",
+  "المهن",
+  "ديانة",
+  "رياضيات",
+  "صفات",
+  "وسائط النقل",
+  "وطنية",
 ];
+
+const english = require
+  .context(
+    "../public/sign language/english",
+    false,
+    /\.(png|jpe?g|svg|jpg|JPG)$/
+  )
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const months = require
+  .context(
+    "../public/sign language/months",
+    false,
+    /\.(png|jpe?g|svg|jpg|JPG)$/
+  )
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const verbs = require
+  .context("../public/sign language/verbs", false, /\.(png|jpe?g|svg|jpg|JPG)$/)
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const house = require
+  .context("../public/sign language/house", false, /\.(png|jpe?g|svg|jpg|JPG)$/)
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const animals = require
+  .context(
+    "../public/sign language/animals",
+    false,
+    /\.(png|jpe?g|svg|jpg|JPG)$/
+  )
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const vegetables = require
+  .context(
+    "../public/sign language/vegetables",
+    false,
+    /\.(png|jpe?g|svg|jpg|JPG)$/
+  )
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const sport = require
+  .context("../public/sign language/sport", false, /\.(png|jpe?g|svg|jpg|JPG)$/)
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const greetings = require
+  .context(
+    "../public/sign language/greetings",
+    false,
+    /\.(png|jpe?g|svg|jpg|JPG)$/
+  )
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const science = require
+  .context(
+    "../public/sign language/science",
+    false,
+    /\.(png|jpe?g|svg|jpg|JPG)$/
+  )
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const arts = require
+  .context("../public/sign language/arts", false, /\.(png|jpe?g|svg|jpg|JPG)$/)
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+// const grammar = require
+//   .context(
+//     "../public/sign language/القواعد والابجدية",
+//     false,
+//     /\.(png|jpe?g|svg|jpg|JPG)$/
+//   )
+//   .keys()
+//   .map((ele) => ele.replace(/\../, ""));
+const computer = require
+  .context(
+    "../public/sign language/computer",
+    false,
+    /\.(png|jpe?g|svg|jpg|JPG)$/
+  )
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const school = require
+  .context(
+    "../public/sign language/school",
+    false,
+    /\.(png|jpe?g|svg|jpg|JPG)$/
+  )
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const profs = require
+  .context("../public/sign language/profs", false, /\.(png|jpe?g|svg|jpg|JPG)$/)
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const relgion = require
+  .context(
+    "../public/sign language/relgion",
+    false,
+    /\.(png|jpe?g|svg|jpg|JPG)$/
+  )
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const math = require
+  .context("../public/sign language/math", false, /\.(png|jpe?g|svg|jpg|JPG)$/)
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const adjectives = require
+  .context(
+    "../public/sign language/adjectives",
+    false,
+    /\.(png|jpe?g|svg|jpg|JPG)$/
+  )
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const transport = require
+  .context(
+    "../public/sign language/transport",
+    false,
+    /\.(png|jpe?g|svg|jpg|JPG)$/
+  )
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+const country = require
+  .context(
+    "../public/sign language/country",
+    false,
+    /\.(png|jpe?g|svg|jpg|JPG)$/
+  )
+  .keys()
+  .map((ele) => ele.replace(/\../, ""));
+
+const englishObj = populateObj(english, "english");
+const monthsObj = populateObj(months, "months");
+const verbsObj = populateObj(verbs, "verbs");
+const houseObj = populateObj(house, "house");
+const vegetablesObj = populateObj(vegetables, "vegetables");
+const sportObj = populateObj(sport, "sport");
+const greetingsObj = populateObj(greetings, "greetings");
+const scienceObj = populateObj(science, "science");
+const artsObj = populateObj(arts, "arts");
+// const grammarObj =populateObj(grammar);
+const computerObj = populateObj(computer, "computer");
+const schoolObj = populateObj(school, "school");
+const relgionObj = populateObj(relgion, "relgion");
+const mathObj = populateObj(math, "math");
+const transportObj = populateObj(transport, "transport");
+const countryObj = populateObj(country, "country");
+const adjectivesObj = populateObj(adjectives, "adjectives");
+
+const all = {
+  ...englishObj,
+  ...monthsObj,
+  ...verbsObj,
+  ...houseObj,
+  ...vegetablesObj,
+  ...sportObj,
+  ...greetingsObj,
+  ...scienceObj,
+  ...artsObj,
+  // ...grammarObj,
+  ...computerObj,
+  ...schoolObj,
+  ...relgionObj,
+  ...mathObj,
+  ...transportObj,
+  ...countryObj,
+  ...adjectivesObj,
+};
 
 export default function Home() {
   const [input, setInput] = useState("");
   const [isPending, startTransition] = useTransition();
   const [filterList, setFilterList] = useState([]);
-  const fuse = new Fuse(words,{
-    includeScore:true,
-    keys:['name']
-  })
-  
-  console.log(fuse.search("بسم الله"))
-  const updateList = (e)=>{
+
+  console.log(JSON.stringify(all));
+
+  const fuse = new Fuse(words, {
+    includeScore: true,
+    keys: ["name"],
+  });
+
+  // console.log(fuse.search("بسم الله"))
+  const updateList = (e) => {
     setInput(e.target.value);
-    startTransition(()=>{
+    startTransition(() => {
       setFilterList(
-        terms.filter((term) => term.indexOf(e.target.value) > -1)
-          )
-     
-    })
-  }
+        categories.filter((term) => term.indexOf(e.target.value) > -1)
+      );
+    });
+  };
   return (
     <div className="container text-center">
       <h2 className="mt-8 text-3xl font-medium">
-        {"Write down what you want to translate".toUpperCase()}
+        {"اكتب ما تبحث عنه".toUpperCase()}
       </h2>
-    
+
+      <img src="/sign language/adjectives/غضبان.JPG" />
       <div dir="rtl" className="my-4 flex justify-center items-center">
         <input
           dir="rtl"
@@ -67,41 +254,18 @@ export default function Home() {
         </svg>
       </div>
       <div className="grid gap-4">
-        {
-          input
-            ? filterList
-                .map((term) => (
-                  <div className="flex flex-col justify-center items-center p-4">
-                    <img src={term + ".JPG"} alt={term} />
-                    <h3 className="text-xl">{term}</h3>
-                    {console.log(isPending)}
-                  </div>
-                ))
-            : null
-        }
+        {input
+          ? filterList.map((term, index) => (
+              <div
+                key={index}
+                className="flex flex-col justify-center items-center p-4"
+              >
+                <img src={term + ".JPG"} alt={term} />
+                <h3 className="text-xl">{term}</h3>
+              </div>
+            ))
+          : null}
       </div>
     </div>
   );
 }
-
-// export async function getStaticProps(){
-//   const query = `
-//   query MyQuery {
-//   words {
-//     name
-//     match
-//     imgs {
-//       id
-//       url
-//     }
-//   }
-// }`;
-
-//   const results = await request(process.env.API,query,{})
-//   return {
-//     props: {
-//       results,
-//     },
-//     revalidate: 60,
-//   };
-// }
